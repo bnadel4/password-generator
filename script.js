a3e784f9e88fca88d396b382f8fdc20999526a49
Jamie Nadel <jamienadel4@gmail.com>
2023-02-12 14:52:06 -0500

    added random string function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+var length;
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -15,7 +16,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function passwordCriteria () {
-var length = prompt('Choose a password length.');
+length = prompt('Choose a password length.');
 console.log('length', length);
 
 if (length >= 8  && length <= 128) {
@@ -26,5 +27,29 @@ else {
   passwordCriteria();
 }
 }
-
 passwordCriteria();
+// write out prompts for the 4 case pref on last one if all no restart 
+var lowercase = prompt('Should it include lowercase letters?');
+console.log('lowercase', lowercase);
+
+var uppercase = prompt('Should it include uppercase letters?');
+console.log('uppercase', uppercase);
+
+var numbers = prompt('Should it include numbers?');
+console.log('numbers', numbers);
+
+var specialCharacters = prompt('Should it include special characters?');
+console.log('specialCharacters', specialCharacters);
+
+//random string function
+
+function generatePassword () {
+   var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+   var charLength = chars.length;
+   var result = '';
+   for ( var i = 0; i < length; i++ ) {
+      result += chars.charAt(Math.floor(Math.random() * charLength));
+   }
+   return result;
+}
+ 
