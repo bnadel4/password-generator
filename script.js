3e00e1ed882bd22a29bf0a831d83ad53360e5773
Jamie Nadel <jamienadel4@gmail.com>
2023-02-14 11:35:01 -0500

    resolving repeated prompts bug

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Variables
 var length = '';
 var lowercase = '';
 var uppercase = '';
@@ -6,45 +6,36 @@ var numbers = '';
 var specialCharacters = '';
 var chars = '';
 var generateBtn = document.querySelector("#generate");
+
+// Event listener
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
+// Prompts user for password criteria
 function writePassword() {
-  chooseCriteria();
-  buildCharacter();
-  revealPassword();
-}
-
-function chooseCriteria() {
 
   length = prompt('Choose a password length.');
 
   if (length >= 8  && length <= 128) {
-  } 
-  else {
+  } else {
     alert('Please enter a number between 8 and 128.')
-    chooseCriteria();
+    writePassword();
   }
 
   lowercase = prompt('Should it include lowercase letters?');
-  console.log('lowercase', lowercase);
-
   uppercase = prompt('Should it include uppercase letters?');
-  console.log('uppercase', uppercase);
-  
   numbers = prompt('Should it include numbers?');
-  console.log('numbers', numbers);
-  
   specialCharacters = prompt('Should it include special characters?');
-  console.log('specialCharacters', specialCharacters);
   
   if (lowercase !== 'yes' && uppercase !== 'yes' && numbers !== 'yes' && specialCharacters !== 'yes') {
-    console.log('User must choose at least one password criteria');
     alert('Please choose at least one character criteria');
-    chooseCriteria();
+    writePassword();
   }
+
+  buildCharacter();
+  revealPassword();
 }
 
+// Builds char string
 function buildCharacter() {
   if (lowercase === 'yes') {
     chars += 'abcdefghijklmnopqrstuvwxyz';
@@ -60,20 +51,20 @@ function buildCharacter() {
   }
 }
 
+// Displays password to the text area element
 function revealPassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
+// Generates the password string based on what the user input
 function generatePassword() {
    var charLength = chars.length;
    var result = '';
    for ( var i = 0; i < length; i++ ) {
       result += chars.charAt(Math.floor(Math.random() * charLength));
    }
-   console.log('chars', chars);
-   console.log('result', result);
    chars = '';
    return result;
 }
